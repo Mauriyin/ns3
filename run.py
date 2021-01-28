42b0980b78c5ac514b5224db0033bdbaff3197ef
haoyin <haoyin@uw.edu>
2021-01-28 18:22:36 -0500

    modify the script to run the power issues

--- run.py
+++ run.py
@@ -9,22 +9,22 @@ freq = 5
 Mcs = 5
 
 for i in range(1):
-    bandwidth = 20 * math.pow(2, 3)
+    bandwidth = 20 * math.pow(2, i)
     for j in range(1):
-        Mcs = 8 + j
-        for k in range(2):
-            dis = 49 + 2*(k + 1)
+        Mcs = 5 + j
+        for k in range(10):
+            dis = 185 + 20 * k 
             while (1):
                 out=os.popen("ps -ef|grep \"./waf --run\"|grep -v grep").read()
                 count = 0
                 for line in out.splitlines():
                     count = count + 1
-                if count < 9:
+                if count < 4:
                     freq = 5
-                    run_command = 'nohup ./waf --run \"scratch/wifi-unequal --standard=11ax  --verbose=0 --maxMpdus=1 --infra=1 --validate=0 --channelWidth={:d}  --phyMode=HeMcs{:d} --frequency={:d} --distance={:d}\" > band{:d}_mcs{:d}_fre{:d}_dis{:d} 2>&1 &'.format(int(bandwidth), int(Mcs), int(freq), int(dis), int(bandwidth), int(Mcs), int(freq), int(dis))
+                    run_command = 'nohup time ./waf --run \"scratch/uplink --standard=11ax  --verbose=0 --duration=200 --maxMpdus=5 --infra=1 --validate=0 --channelWidth={:d}  --phyMode=HeMcs{:d} --frequency={:d} --distance={:d}\" > uplink_data/band{:d}_mcs{:d}_fre{:d}_dis{:d} 2>&1 &'.format(int(bandwidth), int(Mcs), int(freq), int(dis), int(bandwidth), int(Mcs), int(freq), int(dis))
                     os.system(run_command)
                     freq = 6
-                    run_command = 'nohup ./waf --run \"scratch/wifi-unequal --standard=11ax  --verbose=0 --maxMpdus=1 --infra=1 --validate=0 --channelWidth={:d}  --phyMode=HeMcs{:d} --frequency={:d} --distance={:d}\" > band{:d}_mcs{:d}_fre{:d}_dis{:d} 2>&1 &'.format(int(bandwidth), int(Mcs), int(freq), int(dis), int(bandwidth), int(Mcs), int(freq), int(dis))
+                    run_command = 'nohup time ./waf --run \"scratch/uplink --standard=11ax  --verbose=0 --duration=200 --maxMpdus=5 --infra=1 --validate=0 --channelWidth={:d}  --phyMode=HeMcs{:d} --frequency={:d} --distance={:d}\" > uplink_data/band{:d}_mcs{:d}_fre{:d}_dis{:d} 2>&1 &'.format(int(bandwidth), int(Mcs), int(freq), int(dis), int(bandwidth), int(Mcs), int(freq), int(dis))
                     os.system(run_command)
                     break
                 time.sleep(300)
