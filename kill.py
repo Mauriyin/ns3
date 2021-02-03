3ca1002233cb9fefede530e79e8bc0563ad0f28e
haoyin <haoyin@uw.edu>
2021-02-03 18:49:28 -0500

    add script to kill all the process

---
+++ kill.py
@@ -0,0 +1,22 @@
+import os
+import signal
+import time
+import math
+
+out=os.popen("ps -ef|grep \"./waf --run\"|grep -v grep").read()
+for line in out.splitlines():
+    fields = line.split() 
+              
+            # extracting Process ID from the output 
+    pid = fields[1]   
+    # terminating process  
+    os.kill(int(pid), signal.SIGKILL)  
+
+out=os.popen("ps -ef|grep \"uplink\"|grep -v grep").read()
+for line in out.splitlines():
+    fields = line.split() 
+              
+            # extracting Process ID from the output 
+    pid = fields[1]   
+    # terminating process  
+    os.kill(int(pid), signal.SIGKILL)  
